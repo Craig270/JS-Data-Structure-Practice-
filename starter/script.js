5a232f0e7c6fc125ecfe3b66af5af224f8bb930f
Craig Neff <ifly12610@gmail.com>
2021-06-05 22:27:58 -0600

    some small practing around destructuring array. Going to try more with objects next

--- starter/script.js
+++ starter/script.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
 
   openingHours: {
     thu: {
@@ -21,4 +21,26 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+const arr = [2, 3, 4];
+
+const [x, y, z] = arr;
+
+// console.log(x, y, z);
+// console.log(arr);
+
+// let [first, , third] = restaurant.categories;
+
+// console.log(first, third);
+
+// [first, third] = [third, first];
+// console.log(first, third);
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+console.log(starter);
